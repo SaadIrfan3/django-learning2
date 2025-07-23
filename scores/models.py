370d3e1055215e057e751fdf5ea9ebdd8dfc9bd6
saad <saadirfan173@gmail.com>
2025-07-23 13:04:27 +0500

    api stuff

--- scores/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class PlayerScores(models.Model):
-    name = models.CharField(max_length = 100)
-    score = models.IntegerField()
-
-    def __str__(self):
-       return f"{self.name}: {self.score}"
